9f0b08c87f4894b8f0801133766d424c72a40294
Max <109218111+fyflo@users.noreply.github.com>
2024-09-17 10:10:56 +0300

    Add files via upload

---
+++ public/huds/KomodoHUDOld/actions.js
@@ -0,0 +1,15 @@
+/*configs.onChange(data => {
+    if(!data) return;
+    const { panels } = data;
+    $("#players_left #main_primary").text(panels.left_title)
+    $("#players_left #main_secondary").text(panels.left_subtitle)
+    
+    $("#players_right #main_primary").text(panels.right_title)
+    $("#players_right #main_secondary").text(panels.right_subtitle)
+    
+    $("#players_left #box_image").attr("src", `data:image/png;base64,${panels.left_image}`)
+    $("#players_right #box_image").attr("src", `data:image/png;base64,${panels.right_image}`)
+});
+actions.on("toggleAvatars", () => {
+  console.log("Avatars toggled");
+});*/
---
+++ public/huds/KomodoHUDOld/manager.js
@@ -0,0 +1,49 @@
+/*class ActionManager {
+    constructor(){
+        this.listeners = new Map();
+    }
+    execute = (eventName, argument) => {
+        const listeners = this.listeners.get(eventName);
+        if(!listeners) return false;
+        listeners.forEach(callback => {
+            if(argument) callback(argument);
+            else callback();
+        });
+        return true;
+    }
+
+    on = (eventName, listener) => {
+        const listOfListeners = this.listeners.get(eventName) || [];
+        listOfListeners.push(listener);
+        this.listeners.set(eventName, listOfListeners);
+
+        return true;
+    }
+}*/
+class ConfigManager {
+  constructor() {
+    this.listeners = [];
+    this.data = {};
+  }
+  /*save = (data) => {
+    this.data = data;
+    this.execute();
+  };*/
+
+  /*execute = () => {
+    const listeners = this.listeners;
+    if (!listeners || !listeners.length) return false;
+    listeners.forEach((listener) => {
+      listener(this.data);
+    });
+    return true;
+  };*/
+
+  /*onChange = (listener) => {
+    const listOfListeners = this.listeners || [];
+    listOfListeners.push(listener);
+    this.listeners = listOfListeners;
+
+    return true;
+  };*/
+}
